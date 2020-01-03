14333cddda971257c1f5a2bb03761de24b13fd68
Guillaume Gendre <dzb.rtz@gmail.com>
2020-01-03 14:04:03 +0100

    going to androidX

--- src/main/java/io/kristal/pubsubplugin/PubSubReceiver.java
+++ src/main/java/io/kristal/pubsubplugin/PubSubReceiver.java
@@ -31,9 +31,9 @@ package io.kristal.pubsubplugin;
 
 import org.cobaltians.cobalt.fragments.CobaltFragment;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.util.SimpleArrayMap;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.collection.SimpleArrayMap;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
